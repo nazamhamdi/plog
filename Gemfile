3810ec6281ac1469c6ba2140a021b0de31f496da
apprenti <naza23@hotmail.fr>
2014-11-07 11:46:02 +0100

    ajout des fichiers dans le gemfile

--- Gemfile
+++ Gemfile
@@ -5,6 +5,9 @@ ruby '2.1.3'
 gem 'sinatra'
 gem 'thin'
 gem 'rerun'
+gem 'data_mapper'
+gem 'dm-sqlite-adapter'
+gem 'sqlite3'
 group :development do
   gem 'pry'
 end
--- app.rb
+++ app.rb
@@ -1,8 +1,36 @@
+require 'rubygems'
 require 'sinatra'
+require 'data_mapper'
+require 'pry'
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/plog.db")
 
-get '/about' do
+class Post
+  include DataMapper::Resource
+  property :name, Serial
+  property :email, String
+  property :password, String
+end
+DataMapper::Logger.new($stdout, :debug)
+DataMapper.finalize
+Post.auto_upgrade!
+ 
+#get '/' do
+  # get the latest 20 posts
+  #@posts = Post.all(:order => [ :id.desc ], :limit => 20)
+  #erb :about
+#end
+#get '/' do
+ # "Bonjour Simplon "
+  #erb :/about 
+#end
+get '/' do
+  @title ="entrer le text"
   erb :about
 end
-get '/' do
-  "Hello World"
+
+post '/about' do
+  newpost = Post.new(name: params[:name], email: params[:email], password: params[:password])
+  newpost.save
+
+  redirect to '/'
 end
