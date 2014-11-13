e87a102875220b065d1cc88dd6d66626355b0af8
apprenti <naza23@hotmail.fr>
2014-11-13 15:23:48 +0100

    ajout des fichiers del, edit, new, plog,bootstrap et modification au niveua des dossiers layout et about

--- app.rb
+++ app.rb
@@ -6,31 +6,54 @@ DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/plog.db")
 
 class Post
   include DataMapper::Resource
-  property :name, Serial
-  property :email, String
-  property :password, String
+  property :id, Serial
+  property :name, String
+  property :message,String
+  property :created_at, DateTime
 end
 DataMapper::Logger.new($stdout, :debug)
 DataMapper.finalize
 Post.auto_upgrade!
+Time.now.strftime('%d %m  %Y %H:%M')
+
  
 #get '/' do
   # get the latest 20 posts
-  #@posts = Post.all(:order => [ :id.desc ], :limit => 20)
+  @posts = Post.all(:order => [ :id.desc ], :limit => 20)
   #erb :about
 #end
-#get '/' do
- # "Bonjour Simplon "
-  #erb :/about 
-#end
 get '/' do
-  @title ="entrer le text"
+  @title = Post.all(:order => [ :id.desc ], :limit => 20)
   erb :about
 end
 
 post '/about' do
-  newpost = Post.new(name: params[:name], email: params[:email], password: params[:password])
-  newpost.save
+  #newpost = Post.new(name: params[:name],message: params[:message])
+  #newpost.save
+  
+   erb :about
+end
 
-  redirect to '/'
+get '/new' do
+   erb :new
+  
+end
+
+post '/new' do
+  message = Post.create(params[:post])
+  redirect '/'
+end 
+#### Modifier L'un des logs####
+get 'posts:id' do |id|
+@post = Post.get(id)
+erb :edit
+end
+put '/posts/:id' do |id|
+post = Post.get(id)
+success = post.update!(params[:post])
+if success
+redirect "/"
+else
+redirect "/posts/#{id}"
+end
 end
